414bab21c5335791579e7d529d92070178f97094
leonid <soldvds@gmail.com>
2023-03-24 21:00:48 +0300

    Spring gateway service

---
+++ src/main/java/petproject/filmoteka/filmgateway/FilmgatewayApplication.java
@@ -0,0 +1,13 @@
+package petproject.filmoteka.filmgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmgatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmgatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/petproject/filmoteka/filmgateway/FilmgatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package petproject.filmoteka.filmgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilmgatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
